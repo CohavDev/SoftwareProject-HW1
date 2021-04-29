480370810064bd76c32c0504dc9d49b20b64f056
CohavDev <shaulspam@gmail.com>
2021-04-29 18:17:54 +0300

    new classes

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+class Vector:
+    def __init__(self):
+        coordinates = (0,0)
+        clust = Cluster()
 class Cluster:
-    y = 8
+    def __init__(self):
+        self.sum = 0
+        self.count = 0
+        self.median = 0
+    def addVector(vector):
+        x=5
+
