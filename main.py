889e439f2cefcfed4caa765917397d9147c59cfb
CohavDev <shaulspam@gmail.com>
2021-04-30 17:26:35 +0300

    cleaned main

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-class Vector:
-    def __init__(self):
-        coordinates = (0,0)
-        clust = Cluster()
-class Cluster:
-    def __init__(self):
-        self.sum = 0
-        self.count = 0
-        self.median = 0
-    def addVector(vector):
-        x=5
 
